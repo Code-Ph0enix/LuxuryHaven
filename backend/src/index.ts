3c38334f922c547b03bcc4fa0a8e2198885a42c1
Code-Ph0enix <eeshanyajoshi@gmail.com>
2025-03-19 01:57:25 +0530

    final

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: 'http://localhost:5174', 
+  origin: 'https://luxuryhaven-1.onrender.com', 
   credentials: true, 
 }));
 
